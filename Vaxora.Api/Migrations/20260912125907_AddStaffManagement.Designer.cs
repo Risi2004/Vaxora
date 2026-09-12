081d1288a38ecd46f517289f50a91f4fba8c239d
terana-palihawadana <terana.palihawadana@gmail.com>
2026-09-12 18:32:00 +0530

    Add EF migration for staff affiliations and shifts.

--- Vaxora.Api/Migrations/20260912125907_AddStaffManagement.Designer.cs
+++ Vaxora.Api/Migrations/20260912125907_AddStaffManagement.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Vaxora.Api.Data;
@@ -11,9 +12,11 @@ using Vaxora.Api.Data;
 namespace Vaxora.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260912125907_AddStaffManagement")]
+    partial class AddStaffManagement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
